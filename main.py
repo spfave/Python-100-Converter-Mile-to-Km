5d409533f4335a271a34a53bc2b80ba09486c09b
sfave <sebpfave@gmail.com>
2020-12-02 21:51:54 -0500

    implement button and conversion

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tkinter as tk
 
 # Constants
 col0_width = 10
+col1_width = 15
 col2_width = 10
 
 # Variables
@@ -16,6 +17,13 @@ def convert_mile_to_km(miles):
     return kilometers
 
 
+def button_click():
+    """ Get mileage entry and convert to kilometers """
+    miles = float(mile_entry.get())
+    kilometers = convert_mile_to_km(miles)
+    label_km_calc.config(text=str(kilometers))
+
+
 # Main
 window = tk.Tk()
 window.title("Mile to Km Converter")
@@ -23,7 +31,7 @@ window.minsize(width=300, height=50)
 window.config(padx=10, pady=10)
 
 # Mile entry
-mile_entry = tk.Entry()
+mile_entry = tk.Entry(width=col1_width)
 mile_entry.grid(row=0, column=1)
 
 # Labels
@@ -39,5 +47,9 @@ label_km_calc.grid(row=1, column=1)
 label_km = tk.Label(text="Kilometers", anchor="w", width=col2_width)
 label_km.grid(row=1, column=2)
 
+# Button
+convert_button = tk.Button(
+    text="Convert", width=col1_width, command=button_click)
+convert_button.grid(row=2, column=1)
 
 window.mainloop()
