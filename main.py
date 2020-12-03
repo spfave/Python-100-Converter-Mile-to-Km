40bacf30bc45bea7632e2b3367ec2199e6302a24
sfave <sebpfave@gmail.com>
2020-12-02 21:15:54 -0500

    Conversion mile to km function

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+
+# Functions
+def convert_mile_to_km(miles):
+    """ Convert input miles to kilometers and return """
+    kilometers = round(miles*1.609344, 1)
+    return kilometers
+
+
+# Main
